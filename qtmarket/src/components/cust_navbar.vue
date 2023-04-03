b20a38af41833c66e4fc5337fcb2ea732adf2a39
jadatanjq <114057423+jadatanjq@users.noreply.github.com>
2023-04-03 20:27:43 +0800

    -

---
+++ qtmarket/src/components/cust_navbar.vue
@@ -0,0 +1,22 @@
+<style>
+</style>
+
+<template>
+    <div class = 'navbar'>
+        <h1>hi</h1>
+    </div>
+</template>
+
+<script>
+import '../css/main.css'
+
+export default {
+    data() {
+        return {
+        }
+    },
+
+    components: {
+    },
+}
+</script>
---
+++ qtmarket/src/views/home.vue
@@ -0,0 +1,21 @@
+<style>
+</style>
+
+<template>
+    <navbar></navbar>
+</template>
+
+<script>
+import navbar from "@/components/cust_navbar.vue"
+
+export default {
+    data() {
+        return {
+        }
+    },
+
+    components: {
+        navbar,
+    },
+}
+</script>
